963fabce6b7e9cae3987f26b5dc4dc5508abf630
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-02-19 20:00:18 -0600

    01 creado

---
+++ js/01.js
@@ -0,0 +1,24 @@
+//Variables
+
+var producto = 'Audifonos Gamer'; //Iniciar variable y asignarles valor
+
+var disponible; //iniciamos variable sin valor
+
+producto = true; //reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// var 1disponible;
+
+//estilos para las variables
+
+var nombre_producto = 'Monitor HD';
+var nombreProducto = 'Monitor HD'; //camelcase
+var NombreProducto = 'Monitor Hd'; //pascal case para clases
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
